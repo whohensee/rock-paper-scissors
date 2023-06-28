0d23920d26e2d51a44b143e80ad9f68d12196f19
Extav <106775295+whohensee@users.noreply.github.com>
2023-06-28 09:20:19 -0700

    Add logic for playing RPS and returning outcome

--- rps.js
+++ rps.js
@@ -14,3 +14,20 @@ function convertRPStoMod3(sel) {
   }
   return mod3num;
 }
+
+function playSingleRound(pSel, cSel) {
+  let pMod3;
+  let cMod3;
+  let result;
+  pMod3 = convertRPStoMod3(pSel);
+  cMod3 = convertRPStoMod3(cSel);
+
+  if (pMod3 == cMod3) {
+    result = 2; // tie
+  } else if ((pMod3 + 1) % 3 == cMod3) {
+    result = 0; // lose
+  } else {
+    result = 1; // win
+  }
+  return result;
+}
