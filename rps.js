61f60abf0abdd1d8ed3cb85bdd6421b3b2d9c5a6
Extav <106775295+whohensee@users.noreply.github.com>
2023-06-28 18:39:30 -0700

    Add console string explaining outcome

--- rps.js
+++ rps.js
@@ -19,15 +19,26 @@ function playSingleRound(pSel, cSel) {
   let pMod3;
   let cMod3;
   let result;
+  let result_string;
+  let compare_string;
   pMod3 = convertRPStoMod3(pSel);
   cMod3 = convertRPStoMod3(cSel);
 
   if (pMod3 == cMod3) {
     result = 2; // tie
+    result_string = "tie";
+    compare_string = "ties with ";
   } else if ((pMod3 + 1) % 3 == cMod3) {
     result = 0; // lose
+    result_string = "lose";
+    compare_string = "loses to ";
   } else {
     result = 1; // win
+    result_string = "win";
+    compare_string = "beats ";
   }
+  console.log(
+    "You " + result_string + "! " + pSel + " " + compare_string + cSel + "."
+  );
   return result;
 }
